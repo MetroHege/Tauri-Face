158dec3da0b49dbec0f46ab9721fb02a05e09ca7
MetroHege <113589548+MetroHege@users.noreply.github.com>
2025-01-27 11:32:03 +0200

    add gesture recognition feature and integrate with DetectGesture component

---
+++ src/hooks/GestureHooks.ts
@@ -0,0 +1,61 @@
+import { RefObject, useEffect, useState } from "react";
+import { GestureRecognizer, FilesetResolver } from "@mediapipe/tasks-vision";
+import { m } from "node_modules/react-router/dist/development/fog-of-war-BhhVTjSZ.d.mts";
+
+const useGestureRecognition = (videoRef: RefObject<HTMLVideoElement>) => {
+  const [gesture, setGesture] = useState("");
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout> | null = null;
+    let gestureRecognizer: GestureRecognizer | null = null;
+
+    // Initialize the GestureRecognizer
+    const initializeGestureRecognizer = async () => {
+      const filesetResolver = await FilesetResolver.forVisionTasks("/wasm");
+      gestureRecognizer = await GestureRecognizer.createFromOptions(
+        filesetResolver,
+        {
+          baseOptions: {
+            modelAssetPath: "/models/gesture_recognizer.task",
+            delegate: "GPU",
+          },
+          runningMode: "VIDEO",
+          numHands: 1,
+        }
+      );
+    };
+
+    const processVideoFrames = async () => {
+      if (videoRef.current && gestureRecognizer) {
+        const nowInMs = Date.now();
+        const results = gestureRecognizer.recognizeForVideo(
+          videoRef.current,
+          nowInMs
+        );
+        console.log(results);
+      }
+      timer = setTimeout(processVideoFrames, 100);
+    };
+
+    const main = async () => {
+      try {
+        // Wait for the video element to be ready
+        await new Promise<void>((resolve) => {
+          if (videoRef.current!.readyState >= 2) {
+            resolve();
+          } else {
+            videoRef.current!.oncanplay = () => resolve();
+          }
+        });
+
+        await initializeGestureRecognizer();
+        await processVideoFrames();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    main();
+  }, []);
+};
+
+export { useGestureRecognition };
--- src/views/Detected.tsx
+++ src/views/Detected.tsx
@@ -1,19 +1,45 @@
 import { useStore } from "@/stores/DBStore";
-import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { PointerIcon, ThumbsDownIcon, ThumbsUpIcon } from "lucide-react";
 
 const Detected = () => {
+  const navigate = useNavigate();
   const { state } = useLocation();
   const { addFaces } = useStore();
-  console.log("state", state);
-  // store the descriptors in the database
-  try {
-    addFaces(state);
-  } catch (error) {
-    console.error("Error storing face descriptors:", error);
-  }
+  const [time, setTime] = useState(2);
+
+  useEffect(() => {
+    try {
+      addFaces(state);
+    } catch (error) {
+      console.error(error);
+    }
+
+    const interval = setInterval(() => {
+      setTime((prevTime) => {
+        if (prevTime <= 1) {
+          clearInterval(interval);
+          navigate("/gesture/" + state.label);
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-      <h1>Detected</h1>
+    <div className="p-10 text-center text-xl leading-10">
+      <h2 className="text-3xl text-center">{state.label}, prepare to vote</h2>
+      <p>
+        <ThumbsUpIcon className="inline" /> or{" "}
+        <ThumbsDownIcon className="inline" /> to vote,
+      </p>
+      <p>
+        <PointerIcon className="inline" /> to save
+      </p>
+      <p className="text-sm">In {time}s</p>
     </div>
   );
 };
